a65474fbcd6c0b130c0727dd199bc1b979561d9d
pauloesmelos <email>
2023-02-14 03:37:10 -0300

    clienterepository

--- src/main/java/com/delivery/models/repository/ClienteRepository.java
+++ src/main/java/com/delivery/models/repository/ClienteRepository.java
@@ -1,14 +1,16 @@
 package com.delivery.models.repository;
 
 import com.delivery.models.Cliente;
-import com.delivery.models.Produto;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente,Long> {
+public interface ClienteRepository extends JpaRepository<Cliente,Long>{
     List<Cliente> findAll();
     Cliente findById(long idCliente);
 
